9eac25bcca24b80362053d6e85ce8064f16d0885
Hugh Han <hughhan1@users.noreply.github.com>
2015-02-07 17:13:36 -0500

    Deleted create_archive.py
    unnecessary file

--- create_archive.py
+++
@@ -1,8 +0,0 @@
-
-# create_archive.py
-
-from dropbox import client, rest
-
-# Create an archive directory in Dropbox
-if (''' archive does not exist '''):
-	file_create_folder("examplepath/archive");
