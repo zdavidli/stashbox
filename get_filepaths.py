8caf31de9b1b02742c49d25a469666aa1861a5b0
David Li <zdavidli@users.noreply.github.com>
2015-02-07 11:53:11 -0500

    getting full filepaths

---
+++ get_filepaths.py
@@ -0,0 +1,23 @@
+import os
+
+"""
+For each directory in the tree rooted at directory top (including top itself), 
+it yields a 3-tuple (dirpath, dirnames, filenames).
+"""
+
+def get_filepaths(directory):
+    file_paths = []  # List which will store all of the full filepaths.
+
+    # Walk the tree.
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            # Join the two strings in order to form the full filepath.
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)  # Add it to the list.
+
+    return file_paths  # Self-explanatory.
+
+# Run the above function and store its results in a variable.
+
+path = input("Choose a directory: ");
+full_file_paths = get_filepaths(path)
