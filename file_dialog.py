a67380ff3c9a9acd675fd6c0135d5d28bb4fcc30
David Li <zdavidli@users.noreply.github.com>
2015-02-07 23:33:13 -0500

    adding

--- file_dialog.py
+++ file_dialog.py
@@ -4,9 +4,13 @@ import Tkinter, tkFileDialog
 
 w1=[]
 
-root = Tkinter.Tk()
-dirname = tkFileDialog.askdirectory(parent=root,initialdir="~/",title='Please select a directory')
-if len(dirname ) > 0:
-    print "You chose %s" % dirname
-    w1.append(dirname)
-print w1
+def choose_dir():
+    root = Tkinter.Tk()
+    dirname = tkFileDialog.askdirectory(parent=root,initialdir="~/",title='Please select a directory')
+    if len(dirname ) > 0:
+        print "You chose %s" % dirname
+        w1.append(dirname)
+    print w1
+
+if __name__ == '__main__':
+    choose_dir()
